a7ea9e7d92b610bf2d0788e6ecdbb1085cc44071
EnrickyHip <enricky.hipolito@gmail.com>
2023-05-16 10:17:32 -0300

    separate ValidationRuleTest in two files

--- tests/Unit/Abstract/ValidationRuleTest.php
+++ tests/Unit/Abstract/ValidationRuleTest.php
@@ -12,40 +12,22 @@ class TestRule extends ValidationRule
     }
 }
 
+beforeEach(function () {
+    $this->testRule = new TestRule("Error message");
+});
+
 it("should return the correct error message", function () {
-    $testRule = new TestRule("Error message");
-    expect($testRule->getMessage())->toBe("Error message");
+    expect($this->testRule->getMessage())->toBe("Error message");
 });
 
 it("should return false when validation fails", function () {
-    $testRule = new TestRule("Error message");
-    expect($testRule->validate("invalid value"))->toBeFalse();
+    expect($this->testRule->validate("invalid value"))->toBeFalse();
 });
 
 it("should return true when validation fails", function () {
-    $testRule = new TestRule("Error message");
-    expect($testRule->validate("valid value"))->toBeTrue();
+    expect($this->testRule->validate("valid value"))->toBeTrue();
 });
 
 it("should not be a major rule by default", function () {
-    $testRule = new TestRule("Error message");
-    expect($testRule->isMajor())->toBeFalse();
-});
-
-class TestMajorRule extends ValidationRule
-{
-    public function validate(mixed $value): bool
-    {
-        return $value === "valid value";
-    }
-
-    public function isMajor(): bool
-    {
-        return true;
-    }
-}
-
-it("should be a major rule", function () {
-    $testRule = new TestMajorRule("Error message");
-    expect($testRule->isMajor())->toBeTrue();
+    expect($this->testRule->isMajor())->toBeFalse();
 });
