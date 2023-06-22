24438de4b78a3ec1af5fd9cb1462aaf77e705c63
EnrickyHip <enricky.hipolito@gmail.com>
2023-06-22 13:26:47 -0300

    add PHPdocs to Validator.php

--- src/Validator.php
+++ src/Validator.php
@@ -11,6 +11,10 @@ use Enricky\RequestValidator\Abstract\ValidatorInterface;
 use Enricky\RequestValidator\Rules\IsProhibitedRule;
 use Enricky\RequestValidator\Rules\IsRequiredRule;
 
+/**
+ * Internal abstract class for validators. These is a builder class that allow you to add validation rules to attributes.
+ * @internal
+ */
 abstract class Validator implements ValidatorInterface
 {
     /** @var ValidationRule[] $majorRules */
@@ -30,6 +34,11 @@ abstract class Validator implements ValidatorInterface
         $this->attribute = $attribute;
     }
 
+    /**
+     * Require a field.
+     * @param ?string $message optional custom message
+     * @return static Validator insntance to allow chaining another validation rules.
+     */
     public function isRequired(?string $message = null): static
     {
         $rule = new IsRequiredRule($message);
@@ -37,7 +46,12 @@ abstract class Validator implements ValidatorInterface
         return $this;
     }
 
-    /** @param bool|Closure(): bool $condition  */
+    /**
+     * Require a field if a condition is true.
+     * @param bool|Closure(): bool $condition
+     * @param ?string $message optional custom message
+     * @return static Validator insntance to allow chaining another validation rules.
+     */
     public function isRequiredIf(bool|Closure $condition, ?string $message = null): static
     {
         $rule = new IsRequiredRule($message, $condition);
@@ -45,6 +59,12 @@ abstract class Validator implements ValidatorInterface
         return $this;
     }
 
+    /**
+     * Prohibit a field if a condition is true.
+     * @param bool|Closure(): bool $condition
+     * @param ?string $message optional custom message
+     * @return static Validator insntance to allow chaining another validation rules.
+     */
     /** @param bool|Closure(): bool $condition  */
     public function prohibitedIf(bool|Closure $condition, ?string $message = null): static
     {
@@ -53,6 +73,12 @@ abstract class Validator implements ValidatorInterface
         return $this;
     }
 
+
+    /**
+     * Add rule to the attribute.
+     * @param ValidationRule $rule
+     * @return static Validator instance to allow chaining another validation rules.
+     */
     public function addRule(ValidationRule $rule): static
     {
         if ($rule->isMajor()) {
@@ -64,7 +90,10 @@ abstract class Validator implements ValidatorInterface
         return $this;
     }
 
-    /** @return ValidationRule[] */
+    /**
+     * Get all rules added to the attribute.
+     * @return ValidationRule[] array of rules.
+     */
     public function getRules(): array
     {
         return [...$this->rules, ...$this->majorRules];
@@ -75,7 +104,11 @@ abstract class Validator implements ValidatorInterface
         return $this->attribute;
     }
 
-    /** @return string[] */
+
+    /**
+     * Validate and get all attribute errors.
+     * @return string[] array of error messages.
+     */
     public function getErrors(): array
     {
         if ($this->isValid === null) {
@@ -85,6 +118,10 @@ abstract class Validator implements ValidatorInterface
         return $this->errors;
     }
 
+    /**
+     * Validate the attribute.
+     * @return bool true if valid, false otherwise.
+     */
     public function validate(): bool
     {
         $this->isValid = true;
